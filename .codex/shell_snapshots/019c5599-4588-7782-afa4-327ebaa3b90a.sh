5fb30667e0041cf2c2eeeef91777328216ced90d
20011229kk <2939503115@qq.com>
2026-02-14 19:17:38 +0800

    chore: 提交本地配置与工具文件

---
+++ .codex/shell_snapshots/019c5599-4588-7782-afa4-327ebaa3b90a.sh
@@ -0,0 +1,34 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+unalias -a 2>/dev/null || true
+# Functions
+
+# setopts 2
+setopt nohashdirs
+setopt login
+
+# aliases 2
+alias run-help=man
+alias which-command=whence
+
+# exports 20
+export CODEX_HOME=/Users/kk/.codex
+export CODEX_MANAGED_BY_NPM=1
+export DISPLAY=/private/tmp/com.apple.launchd.ROtg6cPixD/org.xquartz:0
+export HOME=/Users/kk
+export LC_CTYPE=UTF-8
+export LOGNAME=kk
+export LaunchInstanceID=0C7625EC-3E49-48F1-97E5-5860A3C87705
+export SECURITYSESSIONID=186a2
+export SHELL=/bin/zsh
+export SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.pvd6O7uvWV/Listeners
+export TERM=xterm-256color
+export TERM_PROGRAM=Apple_Terminal
+export TERM_PROGRAM_VERSION=455.1
+export TERM_SESSION_ID=E3FD72EB-2BEA-4936-A5A1-BB10BB2896FC
+export TMPDIR=/var/folders/q8/b07j831x39z5f93pfpt02tn40000gn/T/
+export USER=kk
+export XPC_FLAGS=0x0
+export XPC_SERVICE_NAME=0
+export __CFBundleIdentifier=com.apple.Terminal
+export __CF_USER_TEXT_ENCODING=0x1F5:0x19:0x34
